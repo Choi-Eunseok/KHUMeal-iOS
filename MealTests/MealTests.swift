89c1b22cd25b022d96277682104a642b13bb8ca0
Choi-Eunseok <ted12333@gmail.com>
2025-12-28 22:21:20 +0900

    Initialize storyboard app

---
+++ MealTests/MealTests.swift
@@ -0,0 +1,17 @@
+//
+//  MealTests.swift
+//  MealTests
+//
+//  Created by 최은석 on 12/28/25.
+//
+
+import Testing
+@testable import Meal
+
+struct MealTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
